6fbfbab451465df4b851efc4495480fe5e4260f9
Abdulmujeeb Ahmad <abdulmujeebahmad03@gmail.com>
2025-11-19 16:03:18 +0100

    Add NestJS configuration for environment variables and GraphQL setup; update dependencies in package.json and package-lock.json

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,
+      context: ({ req }: { req: Request }) => ({ req }),
+      playground: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
