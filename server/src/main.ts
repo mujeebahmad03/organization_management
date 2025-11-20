b72454d6660fafd87ad215f12da98452858a3f88
Abdulmujeeb Ahmad <abdulmujeebahmad03@gmail.com>
2025-11-20 23:33:33 +0100

    Update Swagger API documentation path from 'api' to 'docs' in main.ts

--- server/src/main.ts
+++ server/src/main.ts
@@ -65,7 +65,7 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document, {
+  SwaggerModule.setup('docs', app, document, {
     swaggerOptions: {
       persistAuthorization: true,
       tagsSorter: 'alpha',
